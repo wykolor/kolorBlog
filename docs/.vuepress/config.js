925e52584e5f7ae9fd318ee1026c367da32b7a76
kcolor <1121460147@qq.com>
2019-08-14 20:59:38 +0800

    创建脚本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,39 @@
 module.exports = {
   title: 'kolor',
   description: 'kolor的个人乐园',
+  base: '/vuePress/',
+  dest: './dist', // 设置输出目录
+  repo: 'https://wykolor.github.io/vuePress/',
   themeConfig: {
     // 导航栏
     nav: [{
-      text: 'Home',
-      link: '/'
-    },
-    {
-      text: 'Guide',
-      link: '/'
-    },
-    {
-      text: 'Google',
-      link: 'https://google.com'
-    },
-    {
-      text: 'Languages',
-      items: [{
-        text: 'Chinese',
+        text: 'Home',
         link: '/'
       },
       {
-        text: 'Japanese',
+        text: 'Guide',
         link: '/'
+      },
+      {
+        text: 'Google',
+        link: 'https://google.com'
+      },
+      {
+        text: 'Languages',
+        items: [{
+            text: 'Chinese',
+            link: '/'
+          },
+          {
+            text: 'Japanese',
+            link: '/'
+          }
+        ]
+      },
+      {
+        text: 'Github',
+        link: 'http://www.github.com/wykolor'
       }
-      ]
-    },
-    {
-      text: 'Github',
-      link: 'http://www.github.com/wykolor'
-    }
     ],
   },
   markdown: {
